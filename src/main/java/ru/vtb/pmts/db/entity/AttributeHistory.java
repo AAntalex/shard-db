5e45a3d426317c40b8383ec0ea29037c2ef3fb99
AAntalex <aantalex@gmail.com>
2024-06-05 20:04:31 +0300

    master

--- src/main/java/ru/vtb/pmts/db/entity/AttributeHistory.java
+++ src/main/java/ru/vtb/pmts/db/entity/AttributeHistory.java
@@ -3,17 +3,14 @@ package ru.vtb.pmts.db.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import ru.vtb.pmts.db.entity.abstraction.BaseShardEntity;
-import ru.vtb.pmts.db.model.enums.DataFormat;
-import ru.vtb.pmts.db.model.enums.ShardType;
-import ru.vtb.pmts.db.service.api.DataWrapper;
+
+import java.time.LocalDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class AttributeHistory extends BaseShardEntity {
     private Long entityId;
     private String attributeName;
-    private String data;
-    private DataFormat dataFormat;
-    private ShardType shardType;
-    private DataWrapper dataWrapper;
+    private LocalDateTime time;
+    private String value;
 }
