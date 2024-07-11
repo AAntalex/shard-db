19c187d2abe42e8f35f7053143613a84a9b3450c
AAntalex <aantalex@gmail.com>
2024-07-11 15:52:17 +0300

    master

--- src/main/java/ru/vtb/pmts/db/controller/TestController.java
+++ src/main/java/ru/vtb/pmts/db/controller/TestController.java
@@ -1,25 +1,45 @@
 package ru.vtb.pmts.db.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ru.vtb.pmts.db.model.dto.IndexDto;
+import ru.vtb.pmts.db.service.ShardDataBaseManager;
 import ru.vtb.pmts.db.service.ShardEntityManager;
+
+import java.sql.SQLException;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class TestController {
+    private final ThreadLocal<String> test = new ThreadLocal<>();
+
     private final ShardEntityManager entityManager;
 
     @RequestMapping(
-            method = RequestMethod.GET,
+            method = RequestMethod.POST,
             value = "/api/v1/a",
-            produces = { "application/json" }
+            produces = { "application/json" },
+            consumes = { "application/json" }
     )
-    public ResponseEntity<Void> test() {
-        System.out.println("AAA ");
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<String> test(@RequestBody IndexDto testIdx) {
+        log.info("AAA test = " + test.get());
+        log.info("AAA tesIdx = " + testIdx);
+        log.info("AAA Thread.currentThread().getId() = " + Thread.currentThread().getId());
+
+        try {
+            log.info("AAA dataBaseManager = " + entityManager);
+        } catch (Exception err) {
+            throw new RuntimeException(err);
+        }
+
+        test.set("AAA");
+        return ResponseEntity.ok(entityManager.toString());
     }
 }
---
+++ src/main/java/ru/vtb/pmts/db/service/impl/TransactionalExternalQuery.java
@@ -0,0 +1,61 @@
+package ru.vtb.pmts.db.service.impl;
+
+import ru.vtb.pmts.db.model.enums.QueryType;
+import ru.vtb.pmts.db.service.abstractive.AbstractTransactionalQuery;
+import ru.vtb.pmts.db.service.api.ResultQuery;
+
+import java.sql.*;
+import java.util.*;
+
+public class TransactionalExternalQuery extends AbstractTransactionalQuery {
+    private final List<List<String>> binds = new ArrayList<>();
+    private final List<Class<?>> types = new ArrayList<>();
+    private List<String> currentBinds = new ArrayList<>();
+    private int currentRow;
+
+
+    TransactionalExternalQuery(String query, QueryType queryType) {
+        this.query = query;
+        this.queryType = queryType;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        this.currentRow = 0;
+        this.types.clear();
+        this.currentBinds.clear();
+        this.binds.clear();
+        this.binds.add(this.currentRow, this.currentBinds);
+    }
+
+    @Override
+    public void bindOriginal(int idx, Object o) throws Exception {
+        if (this.currentRow == 0) {
+            this.types.add(idx - 1, o.getClass());
+        }
+        this.currentBinds.add(idx - 1, o.toString());
+    }
+
+    @Override
+    public void addBatchOriginal() throws Exception {
+        this.currentRow++;
+        this.currentBinds = new ArrayList<>();
+        this.binds.add(this.currentRow, this.currentBinds);
+    }
+
+    @Override
+    public int[] executeBatch() throws Exception {
+        return null;
+    }
+
+    @Override
+    public int executeUpdate() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public ResultQuery executeQuery() throws SQLException {
+        return null;
+    }
+}
