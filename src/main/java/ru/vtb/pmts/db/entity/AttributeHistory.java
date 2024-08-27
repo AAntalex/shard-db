2dc51b35fe5ec088d767943a7624885ae53563b4
aantalex <aantalex@gmail.com>
2024-08-27 21:09:54 +0300

    develop

--- src/main/java/ru/vtb/pmts/db/entity/AttributeHistory.java
+++ src/main/java/ru/vtb/pmts/db/entity/AttributeHistory.java
@@ -4,13 +4,13 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import ru.vtb.pmts.db.entity.abstraction.BaseShardEntity;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class AttributeHistory extends BaseShardEntity {
     private Long entityId;
     private String attributeName;
-    private LocalDateTime time;
+    private OffsetDateTime time;
     private String value;
 }
