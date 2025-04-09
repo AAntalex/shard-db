0114fe24303a859bb89a688fd843b82fa4c1230d
aantalex <aantalex@gmail.com>
2025-04-09 19:21:24 +0300

    master

---
+++ src/main/java/com/antalex/db/model/criteria/CriteriaElement.java
@@ -0,0 +1,20 @@
+package com.antalex.db.model.criteria;
+
+import com.antalex.db.model.Cluster;
+import com.antalex.db.model.enums.ShardType;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Accessors(chain = true, fluent = true)
+public class CriteriaElement {
+    private String tableName;
+    private String tableAlias;
+    private List<String> columns = new ArrayList<>();;
+    private List<CriteriaJoin> joins = new ArrayList<>();
+    private Cluster cluster;
+    private ShardType shardType;
+}
---
+++ src/main/java/com/antalex/db/model/criteria/CriteriaJoin.java
@@ -0,0 +1,18 @@
+package com.antalex.db.model.criteria;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.apache.commons.lang3.tuple.Pair;
+
+import javax.persistence.criteria.JoinType;
+import java.util.List;
+
+@Data
+@Accessors(chain = true, fluent = true)
+public class CriteriaJoin {
+    private CriteriaElement element;
+    private String on;
+    private JoinType joinType;
+    private List<Pair<String, String>> joinColumns;
+    private boolean linked;
+}
