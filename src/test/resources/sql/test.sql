a7e0587957c945e7b992f758b855b556ff23d39e
aantalex <aantalex@gmail.com>
2025-05-28 19:08:10 +0300

    master

--- src/test/resources/sql/test.sql
+++ src/test/resources/sql/test.sql
@@ -24,3 +24,24 @@ from T_ACCOUNT acc_dt
          left join t_client cl on acc_dt.C_CLIENT = cl.ID
          join t_client_category cat on cat.ID = cl.c_category
          right join t_payment p on acc_dt.ID = p.c_acc_dt
+
+===========================================
+
+
+select p.ID, p.c_num, acc_dt.C_CODE, acc_Ct.C_CODE, cl_dt.c_name, cl_ct.c_name, cat.c_code
+from t_payment p
+         right join T_ACCOUNT acc_ct on acc_ct.ID = p.c_acc_ct
+         right join T_ACCOUNT acc_dt on acc_dt.ID = p.c_acc_dt
+         left join t_client cl_dt on acc_dt.C_CLIENT = cl_dt.ID
+         left join t_client cl_ct on acc_ct.C_CLIENT = cl_ct.ID
+         left join t_client_category cat on cat.ID = cl_ct.c_category
+         left join t_client_category cat_dt on cat_dt.ID = cl_dt.c_category
+
+select p.ID, p.c_num, acc_dt.C_CODE, acc_Ct.C_CODE, cl_dt.c_name, cl_ct.c_name, cat.c_code
+from T_ACCOUNT acc_dt
+         left join t_payment p on acc_dt.ID = p.c_acc_dt
+         left join T_ACCOUNT acc_ct on acc_ct.ID = p.c_acc_ct
+         left join t_client cl_dt on acc_dt.C_CLIENT = cl_dt.ID
+         left join t_client cl_ct on acc_ct.C_CLIENT = cl_ct.ID
+         left join t_client_category cat on cat.ID = cl_ct.c_category
+         left join t_client_category cat_dt on cat_dt.ID = cl_dt.c_category
